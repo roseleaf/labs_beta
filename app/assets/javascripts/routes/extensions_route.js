d8131a336ba4699aa5f384b20562e47ab2409b3a
Rose Trujillo <roseleaf.red@gmail.com>
2013-06-26 00:15:27 -0700

    first adds isotope

--- app/assets/javascripts/routes/extensions_route.js
+++ app/assets/javascripts/routes/extensions_route.js
@@ -1,38 +1,7 @@
 ZenlabsBeta.ExtensionsRoute = Ember.Route.extend({
 	model: function(){
 		return ZenlabsBeta.Extension.find();
-	},
-	events: {
-	  displayApps: function() {
-	  	this.set('content', ZenlabsBeta.Extension.find({category:'display'}));
-	  	this.render();
-	  },
-	  commentsApps: function(){
-	  	var displayTypeApps = this.filter(function(extension){
-	  		return extension.get('category') == 'comments';
-	  	});
-	  	return displayTypeApps;
-	  }.property('@each.category'),
-	  ticketCreationApps: function(){
-	  	var displayTypeApps = this.filter(function(extension){
-	  		return extension.get('category') == 'ticketCreation';
-	  	});
-	  	return displayTypeApps;
-	  }.property('@each.category'),
-	  findingDataApps: function(){
-	  	var displayTypeApps = this.filter(function(extension){
-	  		return extension.get('category') == 'findingData';
-	  	});
-	  	return displayTypeApps;
-	  }
 	}
 });
 
 
-
-
-// App.PostsRoute = Ember.Route.extend({
-//   model: function() {
-//     return App.Post.find();
-//   }
-// });
--- app/assets/javascripts/views/extensions_view.js
+++ app/assets/javascripts/views/extensions_view.js
@@ -1,3 +1,28 @@
 ZenlabsBeta.ExtensionsView = Ember.View.extend({
+	sidenavView: Ember.View.extend({
+		templateName: 'sidenav',
+		displayFilter: function(){
+		  var $container = $('#widgetlist');
+		  console.log("found container?", $container);
+			$container.isotope({
+			  // options
+			  itemSelector : '.item',
+			  layoutMode : 'fitRows'
+			});
+			$('#filters a').click(function(){
+			  var selector = $(this).attr('data-filter');
+			  $container.isotope({ filter: selector }, function( $items ) {
+				  var len = $items.length;
+				  console.log( 'Isotope has filtered for ' + len + ' items' );
+				});
+			  console.log("yoo?", selector);
 
+			  return false;
+			});
+		},
+		didInsertElement: function(){
+			this.displayFilter();
+
+		}	
+	})
 });
