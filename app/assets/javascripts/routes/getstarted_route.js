a78fabd64b76fd1d51e11f649e6ec5e5bd6e79d7
Rose Trujillo <roseleaf.red@gmail.com>
2013-06-27 19:39:09 -0700

    tweaks to tmplts

--- app/assets/javascripts/routes/getstarted_route.js
+++ app/assets/javascripts/routes/getstarted_route.js
@@ -4,19 +4,7 @@ ZenlabsBeta.GetstartedRoute = Ember.Route.extend({
 	    selectTab: function(name) {
 	      this.controllerFor('getstarted').set('activeTab', name);
 	      this.render(name, { into: 'getstarted', outlet: 'maintab' });
-	    },		
-		showPublic: function(){
-			console.log("pub");
-			this.render('publicapps', {into: 'getstarted', outlet: 'maintab'})
-		},
-		showPrivate: function(){
-			console.log("priv");
-			this.render('privateapps', {into: 'getstarted', outlet: 'maintab'})
-		},
-		showClassic: function(){
-			console.log("clas");
-			this.render('classicapps', {into: 'getstarted', outlet: 'maintab'})	
-		}
+	    }
 
 	},
 
@@ -25,7 +13,7 @@ ZenlabsBeta.GetstartedRoute = Ember.Route.extend({
 		this.render('privateapps', {into: 'getstarted', outlet: 'maintab'});
 	},
 	setupController: function(controller) {
-		controller.set('activeTab', 'home');
+		controller.set('activeTab', 'privateapps');
 	}
 });
 
