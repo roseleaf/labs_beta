19447cf4d5f08301170c5d3532e4a9c4e28b510b
Rose Trujillo <roseleaf.red@gmail.com>
2013-07-02 09:27:21 -0700

    locks api for token access for update, destroy and create

--- app/controllers/api/v1/extensions_controller.rb
+++ app/controllers/api/v1/extensions_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ExtensionsController < ApplicationController
-  respond_to :json
+  respond_to :json    
+  before_filter :restrict_access, :except => [:index, :show]
+
 
   def index
     respond_with Extension.all
@@ -8,74 +10,58 @@ class Api::V1::ExtensionsController < ApplicationController
   def show
     respond_with Extension.find(params[:id])
   end
+
+  def new
+    @extension = Extension.new
+
+    respond_to do |format|
+      format.json { render json: @extension }
+    end
+  end
+
+  def edit
+    @extension = Extension.find(params[:id])
+  end
+
+  def create
+    @extension = Extension.new(params[:extension])
+
+    respond_to do |format|
+      if @extension.save
+        format.json { render json: @extension, status: :created, location: @extension }
+      else
+        format.json { render json: @extension.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @extension = Extension.find(params[:id])
+
+    respond_to do |format|
+      if @extension.update_attributes(params[:extension])
+        format.json { head :no_content }
+      else
+        format.json { render json: @extension.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @extension = Extension.find(params[:id])
+    @extension.destroy
+
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+
+private
+
+  def restrict_access
+    authenticate_or_request_with_http_token do |token, options|
+  	  ApiKey.exists?(access_token: token)
+	end
+  end    
 end
-
-
-# class ExtensionsController < ApplicationController
-
-#   def index
-#     @extensions = Extension.all
-
-#     respond_to do |format|
-#       format.html # index.html.erb
-#       format.json { render json: @extensions }
-#     end
-#   end
-
-#   def show
-#     @extension = Extension.find(params[:id])
-
-#     respond_to do |format|
-#       format.html # show.html.erb
-#       format.json { render json: @extension }
-#     end
-#   end
-
-#   def new
-#     @extension = Extension.new
-
-#     respond_to do |format|
-#       format.json { render json: @extension }
-#     end
-#   end
-
-#   def edit
-#     @extension = Extension.find(params[:id])
-#   end
-
-#   def create
-#     @extension = Extension.new(params[:extension])
-
-#     respond_to do |format|
-#       if @extension.save
-#         format.html { redirect_to @extension, notice: 'Extension was successfully created.' }
-#         format.json { render json: @extension, status: :created, location: @extension }
-#       else
-#         format.json { render json: @extension.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
-
-#   def update
-#     @extension = Extension.find(params[:id])
-
-#     respond_to do |format|
-#       if @extension.update_attributes(params[:extension])
-#         format.html { redirect_to @extension, notice: 'Extension was successfully updated.' }
-#         format.json { head :no_content }
-#       else
-#         format.json { render json: @extension.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
-
-#   def destroy
-#     @extension = Extension.find(params[:id])
-#     @extension.destroy
-
-#     respond_to do |format|
-#       format.html { redirect_to extensions_url }
-#       format.json { head :no_content }
-#     end
-#   end
-# end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130618200846) do
+ActiveRecord::Schema.define(:version => 20130702160351) do
+
+  create_table "api_keys", :force => true do |t|
+    t.string   "access_token"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "extensions", :force => true do |t|
     t.string "name"
