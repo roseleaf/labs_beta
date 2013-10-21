490432e090898bd9028ac139d88aeff33928baea
Rose Trujillo <roseleaf.red@gmail.com>
2013-10-21 15:27:30 -0700

    extracts env vars prep for heroku

--- config/initializers/zendesk_oauth.rb
+++ config/initializers/zendesk_oauth.rb
@@ -1,1 +1,4 @@
 OAUTH_CONFIG = YAML.load_file("#{Rails.root}/config/zendesk_oauth.yml")[Rails.env]
+OAUTH_SECRET = OAUTH_CONFIG[:secret]
+OAUTH_UNIQUE_ID = OAUTH_CONFIG[:unique_id]
+OAUTH_RETURN_URL = OAUTH_CONFIG[:return_url]
